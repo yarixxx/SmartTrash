d89493788751f48e0455104eaf8099c7792c9e3b
Iaroslav Karandashev <yarix7@gmail.com>
2017-09-09 14:03:04 -0700

    Implement report trash

--- app/app.js
+++ app/app.js
@@ -3,16 +3,36 @@
 var TrashCtrl = angular.module('TrashApp', ['ngMaterial', 'ngSanitize']);
 TrashCtrl.controller('TrashCtrl', function($scope, $mdDialog, $mdToast, $mdSidenav) {
 
-  $scope.reportTrashIncident = function() {
-    console.log('trash');
-  }
-  var endpoint = "wss://open-data.api.satori.com";
-  var appKey = "82AE48a0fAbAfCe5eB42f4617DFA6e5e";
-  var channel = "trash";
+  var endpoint = "wss://jgbml2yj.api.satori.com";
+  var appKey = "6Efba84ebC1628e5bcAc67BE0639BE8f";
+  var channel = "trash-channel";
 
   var client = new RTM(endpoint, appKey);
   client.on('enter-connected', function () {
     console.log('Connected to Satori RTM!');
   });
   client.start();
+  
+  $scope.reportTrashIncident = function() {
+    var lat = 37.773615 + (Math.random() / 100);
+    var lon = -122.415987 + (Math.random() / 100);
+    var trash = {
+      who: "client" + Math.ceil(10*Math.random()),
+      type: (Math.ceil(10*Math.random()) % 2) ? "incident" : "trashcan",
+      location: [lat, lon],
+      trashData: {
+        type: (Date.now() % 2) ? "recycle" : "garbage",
+        volume: Math.ceil(10*Math.random()),
+        severity: Math.ceil(10*Math.random())
+      }
+    };
+    client.publish(channel, trash, function(pdu) {
+      if (pdu.action.endsWith("/ok")) {
+        // Publish is confirmed by Satori RTM.
+        console.log("Trash is published: " + JSON.stringify(trash));
+      } else {
+        console.log("Publish request failed: " + pdu.body.error + " - " + pdu.body.reason);
+      }
+    });
+  }
 });
