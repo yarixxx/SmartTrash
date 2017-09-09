d2eb5f0632114cdc0ee98c3001cc37700c095ee2
Iaroslav Karandashev <yarix7@gmail.com>
2017-09-09 13:31:44 -0700

    add trash frame

--- app/app.js
+++ app/app.js
@@ -1,1 +1,19 @@
-console.log('test');
+'use strict';
+
+var TrashCtrl = angular.module('TrashApp', ['ngMaterial', 'ngSanitize']);
+TrashCtrl.controller('TrashCtrl', function($scope, $mdDialog, $mdToast, $mdSidenav) {
+
+  $scope.title = 'Bart Watcher';
+  $scope.status = 'none';
+  $scope.stations = [];
+
+  var endpoint = "wss://open-data.api.satori.com";
+  var appKey = "82AE48a0fAbAfCe5eB42f4617DFA6e5e";
+  var channel = "trash";
+
+  var client = new RTM(endpoint, appKey);
+  client.on('enter-connected', function () {
+    console.log('Connected to Satori RTM!');
+  });
+  client.start();
+});
